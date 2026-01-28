6208f5453b7e376bca4c999769a97291e73c7e8b
Yaser <yasertameem0@gmail.com>
2026-01-28 12:11:12 +0200

    Refactor ProfileController to use UserRepository for user data management

--- lib/features/profile/profile_controller.dart
+++ lib/features/profile/profile_controller.dart
@@ -1,10 +1,9 @@
-import 'dart:io';
-
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:news_app/core/datasource/local_data/preferences_manager.dart';
+import 'package:news_app/core/datasource/local_data/user_repository.dart';
 import 'package:news_app/core/mixins/safe_notify_mixin.dart';
+import 'package:news_app/core/models/user_model.dart';
 
 class ProfileController extends ChangeNotifier with SafeNotify {
   XFile? selectedImage;
@@ -22,15 +21,18 @@ class ProfileController extends ChangeNotifier with SafeNotify {
   }
 
   getUserData() {
-    username = PreferencesManager().getString('username') ?? '';
-    countryName = PreferencesManager().getString('country_name') ;
-    countryCode = PreferencesManager().getString('country_code');
+    final UserModel? user=UserRepository().getUser();
+  username = user?.name ?? '';
+  countryName = user?.countryName;
+  countryCode = user?.countryCode;
     safeNotify();
   }
 
   void saveCountry(Country selectedCountry) async {
-    await PreferencesManager().setString('country_name', selectedCountry.name);
-    await PreferencesManager().setString('country_code', selectedCountry.countryCode);
+    await UserRepository().updateUser(
+      countryName: selectedCountry.name,
+      countryCode: selectedCountry.countryCode,
+    );
     countryName = selectedCountry.name;
     countryCode = selectedCountry.countryCode;
     safeNotify();
